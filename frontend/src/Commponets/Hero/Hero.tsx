8fba6790fddc1f6f8f7ee5a63679c356918b14f4
Trykach34rus <smoke14071995Andres@gmail.com>
2025-09-17 18:06:50 +0300

    [edit] Header styles

--- frontend/src/Commponets/Hero/Hero.tsx
+++ frontend/src/Commponets/Hero/Hero.tsx
@@ -1,34 +1,30 @@
 import catImage from '../../assets/Property 1=Default.png'
 import Vector from '../../assets/Vector.svg'
-import Header from '../Heder/Header'
 import st from './Hero.module.scss'
 
 export default function Hero() {
 	return (
-		<>
-			<section className={st.root}>
-				<Header />
-				<div className={st.row}>
-					<div className={st.textBlock}>
-						<h1>
-							WHERE <br />
-							CREATIVE <br />
-							TEACHING <br />
-							<span className={st.highlight}>THRIVES</span>
-						</h1>
-						<div className={st.buttonContainer}>
-							<img src={Vector} alt='' />
-							<div className={st.ctaButton}>
-								<button className={st.cta}>Sign up</button>
-							</div>
+		<section className={st.root}>
+			<div className={st.row}>
+				<div className={st.textBlock}>
+					<h1>
+						WHERE <br />
+						CREATIVE <br />
+						TEACHING <br />
+						<span className={st.highlight}>THRIVES</span>
+					</h1>
+					<div className={st.buttonContainer}>
+						<img src={Vector} alt='' />
+						<div className={st.ctaButton}>
+							<button className={st.cta}>Sign up</button>
 						</div>
 					</div>
-
-					<div className={st.imageWrapper}>
-						<img src={catImage} alt='Cool Cat' />
-					</div>
 				</div>
-			</section>
-		</>
+
+				<div className={st.imageWrapper}>
+					<img src={catImage} alt='Cool Cat' />
+				</div>
+			</div>
+		</section>
 	)
 }
--- frontend/src/Pages/Main.tsx
+++ frontend/src/Pages/Main.tsx
@@ -1,4 +1,5 @@
 import BenefitsSection from '../Commponets/BenefitsSection/BenefitsSection'
+import Header from '../Commponets/Heder/Header'
 import Hero from '../Commponets/Hero/Hero'
 import Latest from '../Commponets/Latest/Latest'
 import LessonSection from '../Commponets/LessonSection/LessonSection'
@@ -9,6 +10,7 @@ type Props = {}
 export default function Main({}: Props) {
 	return (
 		<div>
+			<Header />
 			<Hero />
 			<LessonSection />
 			<BenefitsSection />
