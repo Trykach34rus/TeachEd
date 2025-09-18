a581e38a77c690455f707385659d6e27cb443902
Trykach34rus <smoke14071995Andres@gmail.com>
2025-09-18 21:25:47 +0300

    [add] Hero Blog,ClassReminder [edit] TeachingJourney

---
+++ frontend/src/Commponets/HeroBlog/HeroBlog.tsx
@@ -0,0 +1,11 @@
+import st from './HeroBlog.module.scss'
+
+export default function HeroBlog() {
+	return (
+		<>
+			<section className={st.root}>
+				<div className={st.row}></div>
+			</section>
+		</>
+	)
+}
--- frontend/src/Pages/Main.tsx
+++ frontend/src/Pages/Main.tsx
@@ -1,10 +1,14 @@
 import BenefitsSection from '../Commponets/BenefitsSection/BenefitsSection'
+import ClassReminder from '../Commponets/ClassReminder/ClassReminder'
+import EnhanceStudents from '../Commponets/EnhanceStudents/EnhanceStudents'
+import Footer from '../Commponets/Footer/Footer'
 import Header from '../Commponets/Heder/Header'
 import Hero from '../Commponets/Hero/Hero'
 import Latest from '../Commponets/Latest/Latest'
 import LessonSection from '../Commponets/LessonSection/LessonSection'
 import TeachingJourney from '../Commponets/TeachingJourney/TeachingJourney'
 import TeachingTools from '../Commponets/TeachingTools/TeachingTools'
+import TestDrivePricing from '../Commponets/TestDrivePricing/TestDrivePricing'
 
 type Props = {}
 
@@ -18,6 +22,10 @@ export default function Main({}: Props) {
 			<TeachingTools />
 			<Latest />
 			<TeachingJourney />
+			<EnhanceStudents />
+			<ClassReminder />
+			<TestDrivePricing />
+			<Footer />
 		</div>
 	)
 }
