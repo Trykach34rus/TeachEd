4da3f5f3334d700929bde6fcce25e92da921b1c4
dinch <ytya228@gmail.com>
2025-09-18 18:29:52 +0300

    [add] ProductsCard

--- frontend/src/Pages/Pricing.tsx
+++ frontend/src/Pages/Pricing.tsx
@@ -1,4 +1,5 @@
 import HeroPricing from "../Commponets/HeroPricing/HeroPricing";
+import ProductsCard from "../Commponets/ProductsCard/ProductsCard";
 import FeaturesPricing from "../Commponets/FeaturesPricing/FeaturesPricing";
 import EnhanceStudents from "../Commponets/EnhanceStudents/EnhanceStudents";
 import TestDrivePricing from "../Commponets/TestDrivePricing/TestDrivePricing";
@@ -10,6 +11,7 @@ export default function Pricing({}: Props) {
   return (
     <div>
       <HeroPricing />
+      <ProductsCard />
       <FeaturesPricing />
       <EnhanceStudents />
       <TestDrivePricing />
