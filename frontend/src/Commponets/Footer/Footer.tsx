3048741e62bbdc946fd2c88128622f23df7b18ae
Trykach34rus <smoke14071995Andres@gmail.com>
2025-09-20 19:18:32 +0300

    [fixed] bags

--- frontend/src/Commponets/Footer/Footer.tsx
+++ frontend/src/Commponets/Footer/Footer.tsx
@@ -1,41 +1,40 @@
-import st from "./Footer.module.scss";
-import logoBlack from "../../assets/TeachEd-Logo (1).svg";
-import bg from "../../assets/background-enhance.png";
+import logoBlack from '../../assets/TeachEd-Logo (1).svg'
+import bg from '../../assets/background-enhance.png'
+import st from './Footer.module.scss'
 
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
 
 const navItems = [
-  { path: '/home', label: 'Home' }
-  { path: "/blog", label: "Blog" },
-  { path: "/events", label: "Events" },
-  { path: "/about", label: "About us" },
-  { path: "/pricing", label: "Pricing" },
-  { path: "/contact", label: "Contact us" },
-];
+	{ path: '/home', label: 'Home' },
+	{ path: '/blog', label: 'Blog' },
+	{ path: '/events', label: 'Events' },
+	{ path: '/about', label: 'About us' },
+	{ path: '/pricing', label: 'Pricing' },
+	{ path: '/contact', label: 'Contact us' },
+]
 
 export default function Footer() {
-  const navigate = useNavigate();
+	const navigate = useNavigate()
 
+	return (
+		<footer className={st.root} style={{ backgroundImage: `url(${bg})` }}>
+			<div className={st.container}>
+				<div className={st.footerContent}>
+					<img src={logoBlack} alt='TeachEd' />
 
-  return (
-      <footer className={st.root} style={{ backgroundImage: `url(${bg})` }}>
-        <div className={st.container}>
-          <div className={st.footerContent}>
-            <img src={logoBlack} alt="TeachEd" />
-
-            <nav className={st.footerMenu}>
-              {navItems.map((item) => (
-                  <button
-                      key={item.path}
-                      onClick={() => navigate(item.path)}
-                      className={st.navItem}
-                  >
-                    {item.label}
-                  </button>
-              ))}
-            </nav>
-          </div>
-        </div>
-      </footer>
-  );
+					<nav className={st.footerMenu}>
+						{navItems.map(item => (
+							<button
+								key={item.path}
+								onClick={() => navigate(item.path)}
+								className={st.navItem}
+							>
+								{item.label}
+							</button>
+						))}
+					</nav>
+				</div>
+			</div>
+		</footer>
+	)
 }
