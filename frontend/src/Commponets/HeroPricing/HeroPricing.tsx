d378bfe5be21a2435bc43572ea3f3222e01e25c6
dinch <ytya228@gmail.com>
2025-09-21 18:55:51 +0300

    [edit] HeroPricing

--- frontend/src/Commponets/HeroPricing/HeroPricing.tsx
+++ frontend/src/Commponets/HeroPricing/HeroPricing.tsx
@@ -1,22 +1,21 @@
-import PricingCat from '../../assets/PricingMainCat.png'
-import st from './HeroPricing.module.scss'
+import PricingCat from "../../assets/PricingMainCat.png";
+import st from "./HeroPricing.module.scss";
 
 export default function HeroPricing() {
-	return (
-		<>
-			<section className={st.root}>
-				<div className={st.row}>
-					<div className={st.container}>
-						<div className={st.pricingCat}>
-							<img src={PricingCat} alt='pricing Cat' />
-						</div>
-						<h1 className={st.titlePricing}>
-							<span>Build educational products </span>
-							on a versatile platform
-						</h1>
-					</div>
-				</div>
-			</section>
-		</>
-	)
+  return (
+    <>
+      <section className={st.root}>
+        <div className={st.container}>
+          <div className={st.pricingCat}>
+            <img src={PricingCat} alt="pricing Cat" />
+          </div>
+          <h1 className={st.titlePricing}>
+            <span>Build educational products </span>
+            <br />
+            on a versatile platform
+          </h1>
+        </div>
+      </section>
+    </>
+  );
 }
