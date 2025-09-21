8ff9cbf5af7348fee8dc6ebfee7cbc0aac7bc6d4
Trykach34rus <smoke14071995Andres@gmail.com>
2025-09-21 15:41:48 +0300

    [add] Burger menu

--- frontend/src/Commponets/Heder/Header.tsx
+++ frontend/src/Commponets/Heder/Header.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import logoWhite from '../../assets/Second-Logo-Black.svg'
 import logoBlack from '../../assets/TeachEd-Logo (1).svg'
+import closeImage from '../../assets/closeImage.svg'
+import burger from '../../assets/iconamoon_menu-burger-horizontal.svg'
 import st from './Header.module.scss'
 
 const navItems = [
@@ -14,6 +16,7 @@ const navItems = [
 ]
 
 export default function Header() {
+	const [burgerOpen, setBurgerOpen] = useState<boolean>(false)
 	const navigate = useNavigate()
 	const location = useLocation()
 	const [isLight, setIsLight] = useState<boolean>(false)
@@ -24,26 +27,30 @@ export default function Header() {
 			return
 		}
 
-		const onScroll = () => {
-			setIsLight(window.scrollY > 0)
-		}
-
+		const onScroll = () => setIsLight(window.scrollY > 0)
 		window.addEventListener('scroll', onScroll)
 		return () => window.removeEventListener('scroll', onScroll)
 	}, [location.pathname])
 
 	return (
 		<header className={`${st.root} ${isLight ? st.light : st.dark}`}>
-			<nav className={st.nav}>
+			<nav className={burgerOpen ? `${st.nav} ${st.nav_active}` : st.nav}>
+				<button className={st.close} onClick={() => setBurgerOpen(false)}>
+					Close <img src={closeImage} alt='' />
+				</button>
 				{navItems.map(item => (
 					<button
 						key={item.path}
-						onClick={() => navigate(item.path)}
+						onClick={() => {
+							navigate(item.path)
+							setBurgerOpen(false)
+						}}
 						className={st.navItem}
 					>
 						{item.label}
 					</button>
 				))}
+				<button className={st.langMobile}>🌐 English</button>
 			</nav>
 
 			<div className={st.logo}>
@@ -53,6 +60,9 @@ export default function Header() {
 			<div className={st.right}>
 				<button className={st.lang}>🌐 English</button>
 				<button className={st.signup}>Sign Up</button>
+				<button className={st.burger} onClick={() => setBurgerOpen(true)}>
+					<img src={burger} alt='Open menu' />
+				</button>
 			</div>
 		</header>
 	)
