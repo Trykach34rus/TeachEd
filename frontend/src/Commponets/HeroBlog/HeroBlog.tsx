47b8cda34ea3bd03cf7de7e6946051f0a6b5a901
Escam01 <danelyanarthur@yandex.com>
2025-09-24 00:01:35 +0300

    [add] BlogPostContent

--- frontend/src/Commponets/HeroBlog/HeroBlog.tsx
+++ frontend/src/Commponets/HeroBlog/HeroBlog.tsx
@@ -1,30 +1,35 @@
-import CatBackground from '../../assets/cat-background.png'
-import st from './HeroBlog.module.scss'
+import CatBackground from "../../assets/cat-background.png";
+import st from "./HeroBlog.module.scss";
 //
 
 export default function HeroBlog() {
-	return (
-		<section
-			className={st.root}
-			style={{ backgroundImage: `url(${CatBackground})` }}
-		>
-			<div className={st.row}>
-				<div className={st.content}>
-					<div className={st.badge}>★ Featured</div>
+  return (
+    <>
+      <section className={st.root}>
+        <div
+          className={st.row}
+          style={{ backgroundImage: `url(${CatBackground})` }}
+        >
+          <div className={st.content}>
+            <div className={st.badge}>★ Featured</div>
 
-					<h1 className={st.title}>
-						AI and you: how AI is changing online education
-					</h1>
+            <h1 className={st.title}>
+              AI and you: how AI is changing online education
+            </h1>
 
-					<p className={st.description}>
-						AI is revolutionizing online education by personalizing learning
-						experiences, automating administrative tasks, and providing
-						real-time feedback. It’s enabling adaptive learning platforms that
-						tailor content to individual needs, making education more accessible
-						and efficient.
-					</p>
-				</div>
-			</div>
-		</section>
-	)
+            <p className={st.description}>
+              AI is revolutionizing online education by personalizing learning
+              experiences, automating administrative tasks, and providing
+              real-time feedback. It’s enabling adaptive learning platforms that
+              tailor content to individual needs, making education more
+              accessible and efficient.
+            </p>
+          </div>
+        </div>
+        <div className={st.blogPostContent}>
+          <p>RECENT BLOG POST</p>
+        </div>
+      </section>
+    </>
+  );
 }
