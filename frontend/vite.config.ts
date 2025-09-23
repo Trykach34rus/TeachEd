819ceefa0082b20c6ba5c98189b13e6aa7d8a718
Trykach34rus <smoke14071995Andres@gmail.com>
2025-09-23 20:50:32 +0300

    [fixed] viteconfig

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/TeachEd/',
 	server: {
 		host: '0.0.0.0',
 		port: 5173,
