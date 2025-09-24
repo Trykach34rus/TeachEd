42e8455282453df8f71fa6248f48053f09988380
dinch <ytya228@gmail.com>
2025-09-24 16:43:34 +0300

    [add] component HeroContact.tsx

--- frontend/src/Pages/ContactUs.tsx
+++ frontend/src/Pages/ContactUs.tsx
@@ -1,5 +1,14 @@
+import Header from "../Commponets/Heder/Header.tsx";
+import HeroContact from "../Commponets/HeroContact/HeroContact.tsx"
+
+
 type Props = {}
 
 export default function ContactUs({}: Props) {
-	return <div>ContactUs</div>
+    return (
+        <div>
+            <Header/>
+            <HeroContact/>
+        </div>
+    )
 }
